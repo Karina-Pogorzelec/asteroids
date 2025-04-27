5714ad17fb77c61ed33597e969cdc4c78170198c
Karina-Pogorzelec <kk.pogorzelec@gmail.com>
2025-04-27 10:40:56 +0200

    player changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -7,14 +8,32 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0  # Initialize delta time to 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0)) #fill with black
+
+        screen.fill((0,0,0)) #fill with black
+
+        updatable.update(dt)
+
+        for item in drawable:
+            item.draw(screen)    
+
         pygame.display.flip()  # display update
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
